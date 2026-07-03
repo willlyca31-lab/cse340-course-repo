93b7977ee3ed986aa5c124ae31b01b9de8979fb4
willlyca31-lab <willlyca31@gmail.com>
2026-07-03 14:03:02 -0600

    add home

---
+++ src/views/server.js
@@ -0,0 +1,18 @@
+
+/**
+ * Routes
+ */
+app.get('/', async (req, res) => {
+    const title = 'Home';
+    res.render('home', { title });
+});
+
+app.get('/organizations', async (req, res) => {
+    const title = 'Our Partner Organizations';
+    res.render('organizations', { title });
+});
+
+app.get('/projects', async (req, res) => {
+    const title = 'Service Projects';
+    res.render('projects', { title });
+});
