85a35e795fdb0b46f1330f9df2d8b33714485cde
willlyca31-lab <willlyca31@gmail.com>
2026-07-10 10:13:48 -0600

    add db

--- src/models/db.js
+++ src/models/db.js
@@ -6,15 +6,7 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+    connectionString: process.env.DB_URL,
 });
 
 export default pool;
-
-pool.on("error", (err) => {
-    console.error("Unexpected database error", err);
-});
