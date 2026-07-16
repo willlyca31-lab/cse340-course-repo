89e815bd5e6d36be93548c74d6b5d26d798bc538
willlyca31-lab <willlyca31@gmail.com>
2026-07-16 10:26:57 -0600

    add pro

---
+++ src/models/projects.js
@@ -0,0 +1,20 @@
+const db = require("./db");
+
+async function getAllProjects() {
+  const sql = `
+    SELECT
+      project.project_id,
+      project.name,
+      project.description,
+      organization.name AS organization_name
+    FROM project
+    JOIN organization
+      ON project.organization_id = organization.organization_id
+    ORDER BY project.project_id;
+  `;
+
+  const result = await db.query(sql);
+  return result.rows;
+}
+
+module.exports = { getAllProjects };
